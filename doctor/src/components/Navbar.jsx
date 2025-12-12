a83e5abd834b1ad38b3403791d3c6456ed01458f
098120 <dikshyantthapa0@gmail.com>
2025-12-12 07:31:53 +0545

    new o#commit

--- doctor/src/components/Navbar.jsx
+++ doctor/src/components/Navbar.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const Navigate = useNavigate();
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img className='w-44 cursor-pointer'src={assets.logo} alt="" />
-      <ul className='flex items-center gap-8'>
-        <NavLink>
-            <li>HOME</li>
-            < hr/>
+      <ul className='hidden md:flex md: flex items-start gap-5 font-medium'>
+        <NavLink to='/'>
+            <li className='py-1 '>HOME</li>
+            < hr className='border-nome outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
-        <NavLink>
-            <li>ALL DOCTORS</li>
-            < hr/>
+        <NavLink to='/doctors'>
+            <li className='py-1'>ALL DOCTORS</li>
+            < hr className='border-nome outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
-        <NavLink>
-            <li>ABOUT</li>
-            < hr/>
+        <NavLink to='/about'>
+            <li className='py-1'>ABOUT</li>
+            < hr className='border-nome outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
-        <NavLink>
-            <li>CONTACT</li>
-            < hr/>
+        <NavLink to='/contact'>
+            <li className='py-1'>CONTACT</li>
+            < hr className='border-nome outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
       </ul>
-      <div>
-        <button>Create account</button>
+      <div className='flex items-center gap-4'>
+        <button onClick={()=>Navigate('/login')}className='bg-primary text-black px-8 py-3 rounded-full font-light hidden md:block'> Create account</button>
       </div>
     </div>
   )
